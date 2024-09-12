234e5b201765e8f59300e0d95fb3db37e9815404
Elias Christensson <Elias.c.christensson@gmail.com>
2024-09-12 10:06:25 +0200

    Update Ui_miniräknare.py

--- Ui_miniräknare.py
+++ Ui_miniräknare.py
@@ -35,20 +35,23 @@ def calculate(a, b, Talhantering):
         Tal_3 = x_float**0.5
         print(Tal_3)
 
+def val(i):
+    print(i)
                                                 #Körs vid klick
 Button_equals = tk.Button(gui, text="=", command=lambda: calculate)
 Button_equals.place(height=50, width = 50, x = 50, y = 50)
-Button_1 = tk.Button(gui, text="1", command=lambda: (1))
+Button_1 = tk.Button(gui, text="1", command=lambda: val(1))
 Button_1.place(height=50, width = 50, x = 100, y = 50)
-Button_2 = tk.Button(gui, text="2", command=lambda: (2))
-Button_2.place(height=50, width = 50, x = 100, y = 50)
-Button_3 = tk.Button(gui, text="3", command=lambda: (3))
-Button_3.place(height=50, width = 50, x = 100, y = 50)
-Button_4 = tk.Button(gui, text="4", command=lambda: (4))
-Button_4.place(height=50, width = 50, x = 150, y = 50)
-Button_5 = tk.Button(gui, text="5", command=lambda: (5))
+Button_2 = tk.Button(gui, text="2", command=lambda: val(2))
+Button_2.place(height=50, width = 50, x = 150, y = 50)
+Button_3 = tk.Button(gui, text="3", command=lambda: val(3))
+Button_3.place(height=50, width = 50, x = 200, y = 50)
+Button_4 = tk.Button(gui, text="4", command=lambda: val(4))
+Button_4.place(height=50, width = 50, x = 100, y = 100)
+Button_5 = tk.Button(gui, text="5", command=lambda: val(5))
 Button_5.place(height=50, width = 50, x = 150, y = 100)
-
+Button_6 = tk.Button(gui, text="6", command=lambda: val(6))
+Button_6.place(height=50, width = 50, x = 200, y = 100)
 
 if __name__ == "__main__":
     gui.mainloop()
